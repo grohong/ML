7510a01fc6c4927ae7f8aa1b999e28f58699de86
Seong ho Hong <grohong76@gmail.com>
2018-03-27 15:15:20 +0900

    logistic classification

--- ML/lab05/load_data_logistic_classification.py
+++ ML/lab05/load_data_logistic_classification.py
@@ -1,5 +1,9 @@
 import tensorflow
 import numpy
 
-xy = numpy.loadtxt()
+xy = numpy.loadtxt('data-03-diabetes.csv', delimiter=', ', dtype=numpy.float32)
+print("==========================")
+print(xy)
+x_data = xy[:, 0:-1]
+y_data = xy[:, -1]
 
